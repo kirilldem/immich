231cee49241b2bec1bd6240d925687c1a02d7df1
mertalev <101130780+mertalev@users.noreply.github.com>
2024-03-17 21:29:04 -0400

    simplified extend

--- server/src/infra/repositories/prisma.repository.ts
+++ server/src/infra/repositories/prisma.repository.ts
@@ -2,15 +2,13 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { metricsExtension } from '../prisma/extensions/metrics';
 
-class ExtendedPrismaClient extends PrismaClient {
+@Injectable()
+export class PrismaRepository extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super();
     return this.$extends(metricsExtension) as this;
   }
-}
 
-@Injectable()
-export class PrismaRepository extends ExtendedPrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
   }
